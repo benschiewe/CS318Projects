b4c0b9e0e96efdd0cf308199d66fe15e3491f7bf
bensc <ben.schiewe@gmail.com>
2023-11-01 16:23:05 -0500

    Fixed comment formatting

--- P04318Schiewe/Source.cpp
+++ P04318Schiewe/Source.cpp
@@ -2,9 +2,12 @@
 #include <random>
 #include <queue>
 
-std::priority_queue<int> generateBaskets(const int min, const int max, const int numBaskets) {	// Returns a priority queue, populated with
-	std::random_device rd;																		// random integers in range [min, max] inclusive
-	std::mt19937 gen(rd());																		// Supposedly mt19937 is more evenly distributed than std::rand()
+/* Returns a priority queue, populated with random integers in range [min, max] inclusive
+   Supposedly mt19937 is more evenly distributed than std::rand() */
+
+std::priority_queue<int> generateBaskets(const int min, const int max, const int numBaskets) {
+	std::random_device rd;
+	std::mt19937 gen(rd());
 	std::uniform_int_distribution<> distr(min, max);
 	std::priority_queue<int> q;
 
@@ -24,18 +27,25 @@ int main() {
 	std::cout << "\n";
 	std::cout << "Enter the number of minutes: ";
 	std::cin >> numMinutes;
-
 	std::cout << "\n";
-	std::priority_queue<int> q{ generateBaskets(MIN_EMERALDS, MAX_EMERALDS, numBaskets) };		// Constructs a new priority queue given the p_queue returned from generateBaskets()
-																								// Some interesting notes...
-	int curr, emeraldCount = 0;																	// I tried passing "q" by reference to generateBaskets to prevent an "expensive" copy op...
-	for (int i = 0; i < numMinutes; i++) {														// But the compiler is smart enough to optimize in either scenario. Very cool
-		curr = q.top();																			// Tested average time with numBaskets, numMinutes = 10,000,000. Both take approx 8750ms
-		emeraldCount += curr;																	// I assume "q" is assigned to the lvalue returned from generateBaskets(),
-		q.pop();																				// since the local var in the helper function goes out of scope following assignment on line 29.
+
+	std::priority_queue<int> q{ generateBaskets(MIN_EMERALDS, MAX_EMERALDS, numBaskets) };
+
+	int curr, emeraldCount = 0;
+	for (int i = 0; i < numMinutes; i++) {
+		curr = q.top();
+		emeraldCount += curr;
+		q.pop();
 		q.push(curr / 2);
 	}
 
 	std::cout << "Dorothy has gathered " << emeraldCount << " emeralds!\n";
 	return 0;
 }
+
+/* Some interesting notes about line 32:
+   I tried passing "q" by reference to generateBaskets() to prevent an expensive copy op
+   But the language / compiler is smart enough to optimize in either scenario. Very cool
+   Tested average execution time with numBaskets, numMinutes = 10,000,000. Both take approx. 8750ms
+   I assume "q" is assigned to the lvalue returned from generateBaskets()
+   since the local var in the helper function geos out of scope following assignment */
