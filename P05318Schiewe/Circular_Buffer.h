dd3b34be048b5de8a207131f4485c0e0aae8e2d5
bensc <ben.schiewe@gmail.com>
2023-11-06 19:48:54 -0600

    wrote most code for buffer implementation

--- P05318Schiewe/Circular_Buffer.h
+++ P05318Schiewe/Circular_Buffer.h
@@ -28,35 +28,47 @@ public:
 	using value_type = T;
 
 	//COMPLETE CONSTRUCTOR BELOW
-	CircularBuffer() {  };
+	CircularBuffer() { _head = 0, _tail = 0, _size = 0, _capacity = cap, _current = 0 };
 
 	//COMPLETE THE FUNCTIONS BELOW USING THE FUNCTION PROTOTYPE GIVEN
 
-	T& head() {}
-	T& tail() {};
-	T const& head() const {};
-	T const& tail() const {};
+	T& head() { return c[_head]; }
+	T& tail() { return c[_tail]; }
+	T const& head() const { return c[_head]; }
+	T const& tail() const { return c[_tail]; }
 
 	//COMPLETE THE push_back function -- circular buffer will overwrite
 	//		oldest data if buffer is full.  This function must update
 	//		appropriate instance variables
-	void push_back(T val) noexcept;
+	void push_back(T val) noexcept {
+		if (_current == _capacity) {	//If we are trying to write out of bounds...
+			_current = 0;				//Set write position to 0
+			_head = _current + 1;		//Update head to 1 after write position
+		};
+		c[_current] = val;				//Update value stored at current position
+		_tail = _current;				//Update tail (newest element) to current
+		_current++;						//Set write position to next element in arr
+	}
 
 	void pop() {
 		if (_size <= 0) {
 			throw std::underflow_error("pop(): empty buffer");
 		}
 		// COMPLETE THE REMAINDER OF THIS FUNCTION
+		else {
+			_current--;
+			_size--;
+		}
 	}
 
 	// COMPLETE THE FUNCTIONS BELOW
-	int size() const noexcept { }
+	int size() const noexcept { return _size; }
 
-	int capacity() noexcept {  }
+	int capacity() noexcept { return _capacity; }
 
-	bool empty() const noexcept {  } //returns true if buffer is empty
+	bool empty() const noexcept { return (_size == 0); } //returns true if buffer is empty
 
-	bool full() const noexcept {  }  //returns true if buffer is full
+	bool full() const noexcept { return (_size == _capacity); }  //returns true if buffer is full
 
 	typename Container::iterator begin() { return c.begin(); }
 
@@ -64,6 +76,12 @@ public:
 
 	// COMPLETE THE OVERLOADED << OPERATOR
 	friend std::ostream& operator<<(std::ostream& os, CircularBuffer& buf) {
+		std::cout << "Buffer Info: head: " << _head << ", tail: " << _tail <<
+			", current: " << _current << ", capacity: " << _capacity << ", size: " << _size << "\n";
+		std::cout << "Container: ";
+		for (T el : c) {
+			std::cout << " " << el;
+		}
 	}
 
 private:
