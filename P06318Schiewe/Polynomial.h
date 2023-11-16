e4066b22daa291298c0de668bc3b834146a65455
bensc <ben.schiewe@gmail.com>
2023-11-15 20:57:03 -0600

    implemented some functions for polynomial class

--- P06318Schiewe/Polynomial.h
+++ P06318Schiewe/Polynomial.h
@@ -3,7 +3,10 @@
 #define __POLYNOMIAL_H_
 #include <iostream>
 #include <list>
-using namespace std;
+#include <cmath>
+#include <algorithm>
+#include <ctype.h>
+#include <string>
 
 ///////////////////////////////////////////////////////// Monomial Class
 // Monomial - class that creates the terms that get inserted in a Polynomial
@@ -11,7 +14,7 @@ using namespace std;
 template <typename NumT>
 class Monomial {
 public:
-	Monomial<NumT>(NumT c = 0, int d = 0) : coeff(c), expo(d){ };
+	Monomial<NumT>(NumT c = 0, int d = 0) : coeff(c), expo(d) { };
 	NumT coefficient(void) const { return coeff; };
 	int degree(void) const { return expo; };
 	void assign_coefficient(const NumT c) { coeff = c; };
@@ -50,7 +53,7 @@ public:
 	}
 
 	// Destructor - use default destructors and list's destructor -- DO NOT MODIFY
-	~Polynomial<NumberType>() {	term_list.clear() }
+	~Polynomial<NumberType>() { term_list.clear(); }
 
 	// Copy Constructor -- DO NOT MODIFY
 	Polynomial<NumberType>(const Polynomial<NumberType>& rhs)
@@ -92,13 +95,13 @@ public:
 	// CALLED FROM ISTREAM AND OSTREAM OVERLOADS
 	// read() function has responsibility for getting input, creating Monomial object,
 	//			and inserting into Polynomial object
-	void read(istream& in = cin);
+	void read(std::istream& in = cin);
 
 	// print() function -- make sure you look back at requirements for this
-	void print(ostream& out = cout) const;
+	void print(std::ostream& out = cout) const;
 
 private:
-	list<Monomial<NumberType> > term_list; //SORTED BY DECREASING DEGREES
+	std::list<Monomial<NumberType> > term_list; //SORTED BY DECREASING DEGREES
 	int number_of_terms;
 	int highest_degree;
 
@@ -111,14 +114,14 @@ private:
 
 // DO NOT MODIFY
 template<typename NumberType>
-istream& operator>>(istream& in, Polynomial<NumberType>& rhs) {
+std::istream& operator>>(std::istream& in, Polynomial<NumberType>& rhs) {
 	rhs.read();
 	return in;
 }
 
 // DO NOT MODIFY
 template<typename NumberType>
-ostream& operator<<(ostream& out, const  Polynomial<NumberType>& rhs) {
+std::ostream& operator<<(std::ostream& out, const  Polynomial<NumberType>& rhs) {
 	rhs.print();
 	return out;
 }
@@ -128,122 +131,178 @@ ostream& operator<<(ostream& out, const  Polynomial<NumberType>& rhs) {
 template<typename NumberType>
 inline const Polynomial<NumberType>& Polynomial<NumberType>::operator=(const Polynomial<NumberType>& rhs)
 {
-	// TODO: insert return statement here
+	term_list = rhs.term_list;
+	number_of_terms = rhs.number_of_terms;
+	highest_degree = rhs.highest_degree;
+	return *this;
 }
 
 template<typename NumberType>
 inline const Polynomial<NumberType>& Polynomial<NumberType>::operator=(Polynomial<NumberType>&& rhs)
 {
-	// TODO: insert return statement here
+	term_list = move(rhs.term_list);
+	number_of_terms = rhs.number_of_terms;
+	highest_degree = rhs.highest_degree;
+	return *this;
 }
 
 template<typename NumberType>
 inline Polynomial<NumberType> Polynomial<NumberType>::operator+=(const Monomial<NumberType>& m)
 {
-	return Polynomial<NumberType>();
+	
 }
 
 template<typename NumberType>
 inline Polynomial<NumberType> Polynomial<NumberType>::operator+=(const Polynomial<NumberType>& rhs)
 {
-	return Polynomial<NumberType>();
+	
 }
 
 template<typename NumberType>
 inline const Polynomial<NumberType> Polynomial<NumberType>::operator+(const Monomial<NumberType>& m) const
 {
-	return Polynomial<NumberType>();
+	
 }
 
 template<typename NumberType>
 inline const Polynomial<NumberType> Polynomial<NumberType>::operator+(const Polynomial<NumberType>& rhs) const
 {
-	return Polynomial<NumberType>();
+	
 }
 
 template<typename NumberType>
 inline Polynomial<NumberType> Polynomial<NumberType>::operator-=(const Monomial<NumberType>& m)
 {
-	return Polynomial<NumberType>();
+	
 }
 
 template<typename NumberType>
 inline Polynomial<NumberType> Polynomial<NumberType>::operator-=(const Polynomial<NumberType>& rhs)
 {
-	return Polynomial<NumberType>();
+	
 }
+
 
 template<typename NumberType>
 inline const Polynomial<NumberType> Polynomial<NumberType>::operator-(const Monomial<NumberType>& m) const
 {
-	return Polynomial<NumberType>();
+	
 }
 
 template<typename NumberType>
 inline const Polynomial<NumberType> Polynomial<NumberType>::operator-(const Polynomial<NumberType>& rhs) const
 {
-	return Polynomial<NumberType>();
+	
 }
 
 template<typename NumberType>
 inline Polynomial<NumberType> Polynomial<NumberType>::operator*=(const Monomial<NumberType>& m)
 {
-	return Polynomial<NumberType>();
+	for (auto it = term_list.begin(); it != term_list.end(); it++)
+	{
+		it->assign_coefficient(it->coefficient() * m.coefficient());
+		it->assign_degree(it->degree() + m.degree());
+	}
+	return *this;
 }
 
 template<typename NumberType>
 inline Polynomial<NumberType> Polynomial<NumberType>::operator*=(const Polynomial<NumberType>& rhs)
 {
-	return Polynomial<NumberType>();
+	for (auto it = term_list.begin(); it != term_list.end(); it++)
+	{
+		for (auto itRhs = rhs.term_list.begin(); itRhs != rhs.term_list.end(); itRhs++)
+		{
+			it->assign_coefficient(it->coefficient() * itRhs->coefficient());
+			it->assign_degree(it->degree() + itRhs->degree());
+		}
+	}
+	return *this;
 }
 
 template<typename NumberType>
 inline const Polynomial<NumberType> Polynomial<NumberType>::operator*(const Monomial<NumberType>& m) const
 {
-	return Polynomial<NumberType>();
+	Polynomial<NumberType> newPoly = *this;
+	newPoly *= m;
+	return newPoly;
 }
 
 template<typename NumberType>
 inline const Polynomial<NumberType> Polynomial<NumberType>::operator*(const Polynomial<NumberType>& rhs) const
 {
-	return Polynomial<NumberType>();
+	Polynomial<NumberType> newPoly = *this;
+	newPoly *= rhs;
+	return newPoly;
 }
 
 template<typename NumberType>
 inline const NumberType Polynomial<NumberType>::evaluate(NumberType x) const
 {
-	return NumberType();
+	NumberType acc = NumberType();
+	for (auto it = term_list.begin(); it != term_list.end(); it++)
+	{
+		acc += it->coefficient() * power(x, it->degree());
+	}
+	return acc;
 }
 
 template<typename NumberType>
 inline bool Polynomial<NumberType>::operator==(const Polynomial<NumberType>& p) const
 {
-	return false;
+	return this->term_list == p.term_list;		//Overloaded in list class - first compares size, then each element
 }
 
 template<typename NumberType>
 inline bool Polynomial<NumberType>::operator!=(const Polynomial<NumberType>& p) const
 {
-	return false;
+	return !(*this == p);
 }
 
 template<typename NumberType>
-inline void Polynomial<NumberType>::read(istream& in)
+inline void Polynomial<NumberType>::read(std::istream& in)
 {
+	const std::streamsize MAX_SIZE = 256;
+	int coeff, degree;
+	std::size_t pos, pos2;
+
+	std::string s;
+	std::getline(in, s);
+
+	while (true)
+	{
+		pos = s.find(" ");
+		pos2 = s.find(" ", pos + 1);
+		coeff = std::stoi(s.substr(0, pos));
+		if (coeff == 0) return;
+		degree = std::stoi(s.substr(pos + 1, pos2));
+		insert_in_poly(*this, Monomial<NumberType>(coeff, degree));
+		s.erase(0, pos2 + 1);
+	}
 }
 
 template<typename NumberType>
-inline void Polynomial<NumberType>::print(ostream& out) const
+inline void Polynomial<NumberType>::print(std::ostream& out) const
 {
+	auto it = term_list.begin();
+	for (it; it != --(--term_list.end()); it++)
+	{
+		out << it->coefficient() << "x^" << it->degree() << " + ";
+	}
+	out << it->coefficient() << "x^" << it->degree() << "\n";
 }
 
 template<typename NumberType>
 inline void Polynomial<NumberType>::insert_in_poly(Polynomial<NumberType>& p, const Monomial<NumberType>& m)
 {
+	auto it = p.term_list.begin();
+	while (it->degree() > m.degree())
+		it++;
+	term_list.insert(it, m);
 }
 
 template<typename NumberType>
 inline NumberType Polynomial<NumberType>::power(NumberType x, int n) const
 {
-	return NumberType();
+	return NumberType(pow(x, n));
 }
